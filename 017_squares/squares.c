c7c036654f1744117f893c24ad1d54d5140c0b6d
ym154 <ym154@duke.edu>
2020-08-27 19:34:33 -0400

    017

--- 017_squares/squares.c
+++ 017_squares/squares.c
@@ -43,9 +43,9 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   for (int y = 0; y < h; y++) {
     //count from 0 to w. Call the number you count with x
     for (int x = 0; x < w; x++) {
-      if (((x >= x_offset && x <= x_offset + size2) &&
+      if (((x >= x_offset && x < x_offset + size2) &&
            (y == y_offset || y == y_offset + size2 - 1)) ||
-          ((y >= y_offset && y <= y_offset + size2) &&
+          ((y >= y_offset && y < y_offset + size2) &&
            (x == x_offset || x == x_offset + size2 - 1))) {
         printf("*");
       }
