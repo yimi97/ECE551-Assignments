4cf4dcbd672a04b9ed1c11464f2de10c03100f56
ym154 <ym154@duke.edu>
2020-09-15 19:57:04 -0400

    041-1

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,6 +4,18 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  if (strcmp(str, "") == 0) {
+    return;
+  }
+  size_t mylen = strlen(str);
+  size_t mid = mylen / 2;
+  // strcpy()
+  // char tmp[mylen];
+  for (int i = 0; i < mid; i++) {
+    char tmp = str[i];
+    str[i] = str[mylen - 1 - i];
+    str[mylen - 1 - i] = tmp;
+  }
 }
 
 int main(void) {
@@ -13,7 +25,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
