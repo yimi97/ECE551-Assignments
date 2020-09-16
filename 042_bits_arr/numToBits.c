ad040a3b307634afa4f6972d8d8b66b3d57c222b
ym154 <ym154@duke.edu>
2020-09-15 21:14:34 -0400

    042-2

--- 042_bits_arr/numToBits.c
+++ 042_bits_arr/numToBits.c
@@ -12,7 +12,7 @@ int getNthBit(uint32_t number, int bit) {
 
 void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
   if (nBits < nNums * 32) {
-    printf("Invalid call to numTobBits! nBits is %d, nNums is %d\n", nBits, nNums);
+    printf("Invalid call to numToBits! nBits is %d, nNums is %d\n", nBits, nNums);
     return;
   }
   for (int i = 0; i < nNums; i++) {
