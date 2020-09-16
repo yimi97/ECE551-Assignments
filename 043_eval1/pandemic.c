d62b654bd2ab8deb90a3f678281bad03a56c9a52
ym154 <ym154@duke.edu>
2020-09-16 19:52:20 -0400

    044-1

---
+++ 043_eval1/pandemic.c
@@ -0,0 +1,27 @@
+#include "pandemic.h"
+
+country_t parseLine(char * line) {
+  //WRITE ME
+  if (line == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  country_t ans;
+  ans.name[0] = '\0';
+  ans.population = 0;
+  return ans;
+}
+
+void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
+  //WRITE ME
+}
+
+void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
+  //WRITE ME
+}
+
+void printCountryWithMax(country_t * countries,
+                         size_t n_countries,
+                         unsigned ** data,
+                         size_t n_days) {
+  //WRITE ME
+}
