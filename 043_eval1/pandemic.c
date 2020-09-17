8f85b8e48dc02f568427adf419f9347aa7b2fbde
ym154 <ym154@duke.edu>
2020-09-16 21:05:51 -0400

    parseLine init

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,13 +1,40 @@
 #include "pandemic.h"
 
+#include <string.h>
+
 country_t parseLine(char * line) {
   //WRITE ME
   if (line == NULL) {
+    // error msg
     exit(EXIT_FAILURE);
   }
+
+  // if wrong type, error msg
+
   country_t ans;
-  ans.name[0] = '\0';
-  ans.population = 0;
+  // ans.name[0] = '\0';
+  // ans.population = 0;
+  char * ptr = line;
+  char pop[100];
+  char myname[64];
+  int i = 0;
+  while (*ptr != ',') {
+    myname[i] = *ptr;
+    ptr++;
+    i++;
+  }
+  myname[i] = '\0';
+  ptr++;
+  i = 0;
+  while (*ptr != '\0') {
+    pop[i] = *ptr;
+    ptr++;
+    i++;
+  }
+
+  // ans.name = myname;
+  strcpy(ans.name, myname);
+  ans.population = atoi(pop);
   return ans;
 }
 
