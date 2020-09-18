2e0c3e52acbdc021fdace40ddb9b670df16e7ad2
ym154 <ym154@duke.edu>
2020-09-18 15:11:39 -0400

    step2 cal running avg

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -8,9 +8,7 @@ country_t parseLine(char * line) {
     // error msg
     exit(EXIT_FAILURE);
   }
-
   // if wrong type, error msg
-
   country_t ans;
   // ans.name[0] = '\0';
   // ans.population = 0;
@@ -37,10 +35,41 @@ country_t parseLine(char * line) {
   ans.population = atoi(pop);
   return ans;
 }
+//unsigned * substring(unsigned * array, size_t start, size_t sub_length, size_t n){
+//unsigned * sub_arr[sub_length];
+//for(size_t i =0;i<sub_length;i++){
+//sub_arr[i] = array[i+start];
+//}
+//}
+
+double cal_avg(unsigned * array, size_t n, unsigned * start, size_t days) {
+  int sum = 0;
+  for (int i = 0; i < days; i++) {
+    sum += *start;
+    start++;
+  }
+  double avg = (double)sum / days;
+  return avg;
+}
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
   //WRITE ME
-  for (int i = 0; i < n_days; i++) {
+  unsigned * ptr = data;
+  double average;
+  int cycle = 0;
+  for (int i = 0; i < n_days; i += 7) {
+    if (n_days - i < 7) {
+      average = cal_avg(data, n_days, ptr, n_days - i);
+      cycle = n_days / 7;
+      avg[cycle] = average;
+      break;
+    }
+    else {
+      average = cal_avg(data, n_days, ptr, 7);
+      ptr += 7;
+      cycle = i / 7;
+      avg[cycle] = average;
+    }
   }
 }
 
