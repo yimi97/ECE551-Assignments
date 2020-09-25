85a92f24cd923d75d70d2cbf88e84a0dc736455b
ym154 <ym154@duke.edu>
2020-09-24 20:11:44 -0400

    test

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 //This function is used to figure out the ordering
 //of the strings in qsort.  You do not need
 //to modify it.
@@ -17,8 +16,38 @@ void sortData(char ** data, size_t count) {
 }
 
 int main(int argc, char ** argv) {
-  
+  if (argc < 1) {  //==1
+    printf("Enter a string?/n");
+    getchar();
+    printf("sss");
+    exit(EXIT_SUCCESS);
+  }
   //WRITE YOUR CODE HERE!
-  
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz;
+  size_t i = 0;
+  FILE * f;
+  for (int numOfFiles = 1; numOfFiles < argc; i++) {
+    f = fopen(argv[numOfFiles], "r");
+    while (getline(&curr, &sz, f) >= 0) {
+      lines = realloc(lines, (i + 1) * sizeof(*lines));
+      lines[i] = curr;
+      curr = NULL;
+      i++;
+    }
+    free(curr);
+    sortData(lines, i);
+    for (size_t j = 0; j < i; j++) {
+      printf("%s", lines[j]);
+      free(lines[j]);
+    }
+    free(lines);
+    //   lines = NULL;
+    //curr = NULL;
+    i = 0;
+  }
+  // FILE * f = fopen("name.txt", "r");
+
   return EXIT_SUCCESS;
 }
