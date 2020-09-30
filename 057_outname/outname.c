8ca0c9ce62a030a04586648fb9f8ba784ca181fb
ym154 <ym154@duke.edu>
2020-09-29 23:03:23 -0400

    057-1

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,21 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * count = ".counts";
+  size_t length = strlen(inputName) + strlen(count);
+  char inputCopy[length + 1];    // +1 for '\0'
+  strcpy(inputCopy, inputName);  // will auto add '\0'?
+  strcat(inputCopy, count);      // will auto start from '\0' and add '\0' at the end.
+
+  char * newName = malloc((length + 1) * sizeof(*newName));
+  for (size_t i = 0; i < length + 1; i++) {
+    newName[i] = inputCopy[i];
+  }
+
+  return newName;
 }
