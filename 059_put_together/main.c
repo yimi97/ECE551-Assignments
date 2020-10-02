1e6a3251dac279475e31a4e1bd84a149f1e5408e
ym154 <ym154@duke.edu>
2020-10-01 22:29:09 -0400

    059-1

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -1,38 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
+
 #include "counts.h"
+#include "kv.h"
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   //WRITE ME
-  return NULL;
+  counts_t * c = createCounts();
+  FILE * f = fopen(filename, "r");
+  char * name = NULL;
+  size_t sz = 0;
+  while (getline(&name, &sz, f) >= 0) {
+    addCount(c, name);
+  }
+  free(name);
+  fclose(f);
+  return c;
 }
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc <= 2) {
+    perror("No list files.\n");
+    exit(EXIT_FAILURE);
+  }
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  kvarray_t * kv = readKVs(argv[1]);
 
- //count from 2 to argc (call the number you count i)
-
+  //count from 2 to argc (call the number you count i)
+  for (int i = 2; i < argc; i++) {
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
-
+    counts_t * c = countFile(argv[i], kv);
     //compute the output file name from argv[i] (call this outName)
-
-
+    char * outName = computeOutputFileName(argv[i]);
     //open the file named by outName (call that f)
-
+    FILE * f = fopen(outName, "w");
     //print the counts from c into the FILE f
-
+    printCounts(c, f);
     //close f
-
+    //    fclose(f); // have already close the file in printCounts. // fclose twice is ok?
     //free the memory for outName and c
-
-
-
- //free the memory for kv
-
+    free(outName);
+    freeCounts(c);
+  }
+  //free the memory for kv
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
