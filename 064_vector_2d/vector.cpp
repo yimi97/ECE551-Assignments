1776cb971b6aa37c0ec5f270414004a5f5a1819e
ym154 <ym154@duke.edu>
2020-10-06 14:58:14 -0400

    064-1

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -5,3 +5,11 @@
 
 /* write your class implementation in this file
  */
+/*
+Vector2D operator+(const Vector2D & rhs) const {
+  Vector2D ans;
+  ans.x = x + rhs.x;
+  ans.y = y + rhs.y;
+  return ans;
+}
+*/
--- 064_vector_2d/vector.hpp
+++ 064_vector_2d/vector.hpp
@@ -1,3 +1,36 @@
 /* write your class interface in this file
    write function definitions only if they are very short
  */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cmath>
+
+using namespace std;
+class Vector2D {
+ private:
+  double x;
+  double y;
+
+ public:
+  void initVector(double newx, double newy) {
+    x = newx;
+    y = newy;
+  }
+
+  double getMagnitude() const { return sqrt(x * x + y * y); }
+
+  Vector2D operator+(const Vector2D & rhs) const {
+    Vector2D ans;
+    ans.x = x + rhs.x;
+    ans.y = y + rhs.y;
+    return ans;
+  }
+  Vector2D & operator+=(const Vector2D & rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    return *this;
+  }
+  double dot(const Vector2D & rhs) const { return x * rhs.x + y * rhs.y; }
+  void print() const { printf("<%.2f, %.2f>", x, y); }
+};
