4623137ad95368283d93b411aa41670c78a0678e
ym154 <ym154@duke.edu>
2020-10-13 22:07:59 -0400

    074-1

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -1,37 +1,82 @@
 #include "IntArray.h"
+
 #include <assert.h>
+
 #include <ostream>
 
-IntArray::IntArray() {
-
-}
-IntArray::IntArray(int n) {
-
+IntArray::IntArray() : data(NULL), numElements(0) {
 }
 
-IntArray::IntArray(const IntArray & rhs)  {
+IntArray::IntArray(int n) : data(new int[n]), numElements(n) {
+}
 
+IntArray::IntArray(const IntArray & rhs) {
+  data = new int[rhs.numElements];
+  numElements = rhs.numElements;
+  for (int i = 0; i < numElements; i++) {
+    data[i] = rhs.data[i];
+  }
 }
 IntArray::~IntArray() {
-
+  delete[] data;
 }
 
 IntArray & IntArray::operator=(const IntArray & rhs) {
+  if (this != &rhs) {
+    delete[] data;
+    data = new int[rhs.numElements];
+    numElements = rhs.numElements;
+    for (int i = 0; i < numElements; i++) {
+      data[i] = rhs.data[i];
+    }
+  }
+  return *this;
 }
 const int & IntArray::operator[](int index) const {
+  assert(index < numElements && index >= 0);
+  return data[index];
 }
 int & IntArray::operator[](int index) {
+  assert(index < numElements && index >= 0);
+  return data[index];
 }
 
 int IntArray::size() const {
+  return numElements;
 }
 
 bool IntArray::operator==(const IntArray & rhs) const {
+  if (numElements != rhs.numElements) {
+    return false;
+  }
+  for (int i = 0; i < numElements; i++) {
+    if (data[i] != rhs.data[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 bool IntArray::operator!=(const IntArray & rhs) const {
+  if (numElements != rhs.numElements) {
+    return true;
+  }
+  for (int i = 0; i < numElements; i++) {
+    if (data[i] != rhs.data[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-
+  s << "{";
+  for (int i = 0; i < rhs.size() - 1; i++) {
+    s << rhs[i] << ",";
+  }
+  if (rhs.size() - 1 >= 0) {
+    s << rhs[rhs.size() - 1];
+  }
+  s << "}";
+  return s;
 }
