265dc8c716c3db5f0b7ba79d396bb6922d27222a
ym154 <ym154@duke.edu>
2020-11-10 21:22:58 -0500

    103-1

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
+
+void buildMapHelper(BitString b, std::map<unsigned, BitString> & theMap, Node * curr) {
+  if (curr == NULL) {
+    return;
+  }
+  if (curr->sym != NO_SYM) {
+    assert(curr->left == NULL && curr->right == NULL);
+    theMap.insert(std::pair<unsigned, BitString>(curr->sym, b));
+  }
+  else {
+    buildMapHelper(b.plusZero(), theMap, curr->left);
+    buildMapHelper(b.plusOne(), theMap, curr->right);
+  }
 }
 
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  //WRITE ME!
+  buildMapHelper(b.plusZero(), theMap, left);
+  buildMapHelper(b.plusOne(), theMap, right);
+}
