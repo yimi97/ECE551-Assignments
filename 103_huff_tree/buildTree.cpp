5532e6e9755a5767a2a97855cb9c37e99b17e38a
ym154 <ym154@duke.edu>
2020-11-10 20:52:14 -0500

    103-1

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,20 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (unsigned i = 0; i < 257; i++) {
+    if (counts[i] != 0)
+      pq.push(new Node(i, counts[i]));
+  }
+
+  while (pq.size() >= 2) {
+    Node * first = pq.top();
+    pq.pop();
+    Node * second = pq.top();
+    pq.pop();
+    Node * new_node = new Node(first, second);
+    pq.push(new_node);
+  }
+  return pq.top();
+}
